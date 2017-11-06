54cb123784c6553be9689193953f03e8236fbd20
Reddel Gastelum <reddelgastelum@gmail.com>
2017-11-06 08:35:58 -0800

    Add controller directory

---
+++ app/controllers/todo.controller.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose');
+var Nexmo = require('nexmo');
+var Todo = require('../models/todo');
+
+// Text messaging functionality
+var nexmo = new Nexmo({
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET
+});
+
+module.exports = {
+  getAllTodos: function(req, res) {
+    Todo.find({}).sort('dueDate').exec(function(err, todos) {
+
+      if (todos.length > 0) {
+        for (var i = 0; i < todos.length; i++) {
+          var d = new Date(todos[i].dueDate);
+          todos[i].date = (d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear();
+        }
+      }
+
+      res.render('index', {
+        todos: todos
+      });
+    });
+  },
+  newTodo: function(req, res) {
+    console.log(req.body.dueDate);
+    var todo = new Todo({
+      title: req.body.title,
+      dueDate: new Date(req.body.dueDate),
+      details: req.body.details
+    });
+
+    todo.save(function() {
+      // Send Text Message
+      nexmo.message.sendSms(process.env.VIRTUAL_NUMBER, '16266026587', req.body.title+' ',
+        (err, responseData) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.dir(responseData);
+          }
+        }
+      );
+
+      res.redirect('/todos');
+    });
+  },
+  deleteTodo: function(req, res) {
+    Todo.remove({_id:req.params.id}, function(err, todo) {
+      res.redirect('/todos');
+    });
+  }
+}
--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,62 +1,14 @@
 var router = require('express').Router();
-var mongoose = require('mongoose');
-var Nexmo = require('nexmo');
-var Todo = require('../models/todo');
-
-var nexmo = new Nexmo({
-  apiKey: process.env.NEXMO_API_KEY,
-  apiSecret: process.env.NEXMO_API_SECRET
-});
+var todoController = require('../controllers/todo.controller');
 
 // Todos
-router.get('/', function(req, res) {
-  Todo.find({}).sort('dueDate').exec(function(err, todos) {
-
-    if (todos.length > 0) {
-      for (var i = 0; i < todos.length; i++) {
-        var d = new Date(todos[i].dueDate);
-        todos[i].date = (d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear();
-      }
-    }
-
-    res.render('index', {
-      todos: todos
-    });
-  });
-});
+router.get('/', todoController.getAllTodos);
 
 // New Todo
-router.post('/', function(req, res) {
-  console.log(req.body.dueDate);
-  var todo = new Todo({
-    title: req.body.title,
-    dueDate: new Date(req.body.dueDate),
-    details: req.body.details
-  });
-
-
-
-  todo.save(function() {
-
-    nexmo.message.sendSms(process.env.VIRTUAL_NUMBER, '16266026587', req.body.title+' ',
-      (err, responseData) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.dir(responseData);
-        }
-      }
-   );
-
-    res.redirect('/todos');
-  });
-});
+router.post('/', todoController.newTodo);
 
 // Delete Todo
-router.get('/delete/:id', function(req, res) {
-  Todo.remove({_id:req.params.id}, function(err, todo) {
-    res.redirect('/todos');
-  });
-});
+router.get('/delete/:id', todoController.deleteTodo);
+
 
 module.exports = router;
